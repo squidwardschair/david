5d1e3dfb5bebde72016e5c6a76d3f2c03a3929d7
squidwardschair <hudsonzhao1@gmail.com>
2023-11-25 14:40:49 -0500

    thanksgiving update

---
+++ navbar.js
@@ -0,0 +1,9 @@
+function responsiveNavbar() {
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
+  
